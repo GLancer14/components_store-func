8eb8b52321607709523da6760865e77eb14f9f24
unknown <181259602+GLancer14@users.noreply.github.com>
2025-06-16 11:41:20 +0400

    Corrected code-style

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import ShopItemFunc from './ShopItemFunc';
 function App() {
   return (
     <div className="container">
-      <div className="background-element">
-      </div>
+      <div className="background-element"></div>
       <div className="highlight-window">
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
         <ShopItemFunc item={item} />
       </div>
-  </div>
+    </div>
   );
 }
 
